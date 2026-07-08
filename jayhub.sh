e258ee230b2ff8ec83a3443905aecb5781379087
RhancisDevs <129867977+RhancisDevs@users.noreply.github.com>
2026-07-08 15:32:48 +0800

    Create jayhub.sh

---
+++ jayhub.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+termux-setup-storage && \
+yes | pkg update && \
+yes | pkg upgrade && \
+yes | pkg install python python-pip tsu libexpat openssl -y && \
+yes | pkg install python-psutil && \
+pip install requests Flask colorama aiohttp psutil crypto pycryptodome loguru prettytable && \
+curl -Ls "https://raw.githubusercontent.com/RhancisDevs/nonse/main/weather-rejoiner.py" -o /sdcard/Download/weather-rejoiner.py
