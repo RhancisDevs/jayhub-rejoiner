ceab06cbcb10f9687b8df65b5d0e0e5e23855339
RhancisDevs <129867977+RhancisDevs@users.noreply.github.com>
2026-07-08 15:36:06 +0800

    Update jayhub.sh

--- jayhub.sh
+++ jayhub.sh
@@ -7,3 +7,10 @@ yes | pkg install python python-pip tsu libexpat openssl -y && \
 yes | pkg install python-psutil && \
 pip install requests Flask colorama aiohttp psutil crypto pycryptodome loguru prettytable && \
 curl -Ls "https://raw.githubusercontent.com/RhancisDevs/nonse/main/weather-rejoiner.py" -o /sdcard/Download/weather-rejoiner.py
+
+echo ""
+echo "=================================="
+echo " JayHub Weather Rejoiner Installed"
+echo "=================================="
+echo ""
+echo "Run it with second command to start!"
